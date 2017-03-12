f9a9e2102ebe9817a788f01161c558ba9870d9bd
Krishel Lasam <krishel.lasam@gmail.com>
2017-03-12 07:37:28 -0400

    add food validation

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,5 @@ class Food < ApplicationRecord
   has_many :restaurant_foods
   has_many :restaurants, through: :restaurant_foods, :foreign_key => :restaurant_id
   accepts_nested_attributes_for :restaurant_foods
+  validates_presence_of :name
 end
